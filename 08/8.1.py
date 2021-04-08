e03751b6703b837ecfbd36268a76ac78455ff83a
cainnn <cainnn@126.com>
2021-04-08 20:26:41 +0800

    2

--- 08/8.1.py
+++ 08/8.1.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
-img = np.zeros((5,5),np.uint8)
+
+img = np.zeros((5, 5), np.uint8)
 # img = np.arange(25).reshape(5,5)
 
 # arr = np.arange(1,26).reshape(5,5)
@@ -8,9 +9,9 @@ img = np.zeros((5,5),np.uint8)
 # print(img)
 # img = np.reshape(img,(5,5))
 # print(img)
-img[1:4,1:4] = 1
-kernel = np.ones((3,1),np.uint8)
-erosion = cv2.erode(img,kernel)
-print("img=\n",img)
-print("kernel=\n",kernel)
-print("erosion=\n",erosion)
+img[1:4, 1:4] = 1
+kernel = np.ones((3, 1), np.uint8)
+erosion = cv2.erode(img, kernel)
+print("img=\n", img)
+print("kernel=\n", kernel)
+print("erosion=\n", erosion)
